e9aedd9c3341b30cb1c3e15f82f78bee9c2a390c
Shangxin Guo <int123c@gmail.com>
2018-07-21 11:12:19 +0800

    Add weaklet and weakvar

--- Swiftier/Classes/SwiftierLetAndVar.h
+++ Swiftier/Classes/SwiftierLetAndVar.h
@@ -13,4 +13,16 @@
 #define var __auto_type
 #endif
 
+#if defined(__cplusplus)
+#define weaklet __weak auto const
+#else
+#define weaklet __weak __auto_type const
+#endif
+
+#if defined(__cplusplus)
+#define weakvar __weak auto
+#else
+#define weakvar __weak __auto_type
+#endif
+
 #endif /* LetAndVar_h */
